68d3f1353ae090b9a59113e7e367fb862ffd32cc
Mauricio Gualteros <mgp.94@hotmail.com>
2022-01-13 22:00:17 -0300

    ULTIMO COMMIT ANTES DE SUBIR

---
+++ app.js
@@ -0,0 +1,12 @@
+// const video_mp4 = document.getElementById('video'),boton_play=document.getElementById('play'),boton_pause=document.getElementById('pause');
+
+// boton_play.addEventListener('click',()=>{
+//     video_mp4.play()
+// })
+
+// boton_pause.addEventListener('click',()=>{
+//     video_mp4.pause()
+// })
+
+//MANIPULACION DEL DOM - OPERACIONES MULTIPLES
+d
